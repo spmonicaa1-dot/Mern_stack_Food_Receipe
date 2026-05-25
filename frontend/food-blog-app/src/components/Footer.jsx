526e8835076c16efae9e9ee9792abe9a8eae6576
Monica <spmonicaa1@gmail.com>
2026-05-25 14:44:00 +0530

    Initial commit

---
+++ frontend/food-blog-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <p>© 2025 Monicaa’s FoodRecipe App | All Rights Reserved</p>
+    </div>
+  )
+}
